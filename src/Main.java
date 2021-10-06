9fee99eefb1db122a3beefaf1900740b797f4102
Michal1218 <90755518+Michal1218@users.noreply.github.com>
2021-10-06 20:45:24 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] items = {"Chleba", "Rohlík", "Rýže", "Jablko", "Kofola", "Twix", "Kit-Kat", "Toustový chléb", "Hovězí maso"};
+        int[] itemsCount = new int[items.length];
+        int total = 0;
+
+        for (int i = 0; i < itemsCount.length; i++) {
+            System.out.println(i + 1 + " = " + items[i]);
+        }
+        System.out.format("%n" + "Co si dnes dáte?: " + "%n");
+
+        int num = sc.nextInt();
+        String s = Integer.toString(num);
+        int[] intArray = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            intArray[i] = Character.getNumericValue(s.charAt(i));
+        }
+        int[] choose;
+        choose = intArray;
+
+        for (int j = 0; j < choose.length; j++) {
+            int k = choose[j];
+
+            switch (k) {
+                case 1:
+                    total += 30;
+                    itemsCount[0]++;
+                    break;
+
+                case 2:
+                    total += 2;
+                    itemsCount[1]++;
+                    break;
+
+                case 3:
+                    total += 38;
+                    itemsCount[2]++;
+                    break;
+
+                case 4:
+                    total += 10;
+                    itemsCount[3]++;
+                    break;
+
+                case 5:
+                    total += 25;
+                    itemsCount[4]++;
+                    break;
+
+                case 6:
+                    total += 15;
+                    itemsCount[5]++;
+                    break;
+                case 7:
+                    total += 23;
+                    itemsCount[6]++;
+                    break;
+                case 8:
+                    total += 50;
+                    itemsCount[7]++;
+                    break;
+                case 9:
+                    total += 180;
+                    itemsCount[8]++;
+                    break;
+
+                default:
+                    System.out.println("Neznámá položka");
+                    break;
+            }
+        }
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(itemsCount[i] + "x" + " " + items[i]);
+        }
+        System.out.format("%n" + "Total: " + total + " Kč");
+    }
+}
+
+
